e5c3334347a633d1de063ad860238ba061d56362
glynn0 <62221548+glynn0@users.noreply.github.com>
2020-03-15 19:40:34 -0300

    Create triplex.cpp

---
+++ triplex.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "You are a secret agent breaking into a secure server room";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to contirnue...";
+    std::cout << std::endl;
+
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+    
+    // Hello
+    
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << sum << std::endl;;
+     
+    std::cout << product << std::endl;;
+    
+    return 0;
+}
