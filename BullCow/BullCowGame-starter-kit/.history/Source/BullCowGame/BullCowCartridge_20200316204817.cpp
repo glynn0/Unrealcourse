bca5b18d00e86f865ade2555d1903fbab2b4a381
glynn0 <glynn0@eastlink.ca>
2020-03-16 20:49:50 -0300

    Trying to Setup VS CODE

--- BullCow/BullCowGame-starter-kit/.history/Source/BullCowGame/BullCowCartridge_20200316204817.cpp
+++ BullCow/BullCowGame-starter-kit/.history/Source/BullCowGame/BullCowCartridge_20200316204817.cpp
@@ -5,7 +5,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Hello World!!!"));
-    
+    Pr
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
